fb6626779525f364d971a33549a1db7d7a46f0f5
eburillo <eburillo85@gmail.com>
2016-11-18 17:30:14 +0100

    Update repos-reducer.js

--- src/reducers/repos-reducer.js
+++ src/reducers/repos-reducer.js
@@ -1,107 +1,42 @@
-import React from 'react';
-import actionTypes from '../actions/action-types.js';
+import ActionTypes from '../actions/action-types.js'
 import ErrorMessages from '../messages/errors.js';
-import reposReducer from '../reducers/repos-reducer.js';
 
-describe('Testing Reducers', function() {
-  describe('Repositories', function() {
+const initialState = {
+	repos: {
+		message: "",
+		list: []
+	}
+}
 
-	   describe('default', function() {
-  		it('returns the initial state', function() {
-  			expect(reposReducer()).toEqual({
-          repos: {
-            message: "",
-            list: []
-          }
-        });
-  		});
-  	});
+const reposReducer = function(state = initialState, action) {
+	if (typeof action === 'undefined') {
+		return initialState;
+	}
+	switch (action.type) {
+		case ActionTypes.GET_REPOS_SUCCESS:
+			return {
+				repos: {
+					message: "",
+					list: action.repos
+				}
+			};
+		case ActionTypes.GET_NO_REPOS:
+			return {
+				repos: {
+					message: ErrorMessages.GET_NO_REPOS,
+					list: []
+				}
+			};
+		case ActionTypes.GET_REPOS_FAILED:
+				return {
+					repos: {
+						message: ErrorMessages.GET_REPOS_FAILED,
+						list: []
+					}
+				};
+		default:
+			return state;
+	}
+}
 
-  	describe('on GET_REPOS_SUCCESS action', function() {
-  		var state, newState, response;
-
-  		beforeEach(function() {
-  			state = {
-          repos: {
-            message: "",
-            list: []
-          }
-  			}
-        newState = {
-          repos: {
-            message: "",
-            list: [
-              {
-                "name": "atomic-design",
-                "description": "The repository for the Atomic Design book",
-                "html_url": "https://github.com/bradfrost/atomic-design"
-              }
-            ]
-          }
-  			}
-        response = {
-            repos: [
-              {
-                "name": "atomic-design",
-                "description": "The repository for the Atomic Design book",
-                "html_url": "https://github.com/bradfrost/atomic-design"
-              }
-            ]
-        }
-  		});
-
-  		it('returns the state with the new repos founded', function() {
-  			expect(reposReducer(state, {type: actionTypes.GET_REPOS_SUCCESS, repos: response.repos})).toEqual(newState)
-  		});
-
-	   });
-
-     describe('on GET_NO_REPOS action', function() {
-   		var state, newState, response;
-
-   		beforeEach(function() {
-   			state = {
-           repos: {
-             message: "",
-             list: []
-           }
-   			}
-        newState = {
-           repos: {
-             message: ErrorMessages.GET_NO_REPOS,
-             list: []
-           }
-   			}
-   		});
-
-   		it('returns the state when no repos where founded', function() {
-   			expect(reposReducer(state, {type: actionTypes.GET_NO_REPOS})).toEqual(newState)
-   		});
-
- 	   });
-
-     describe('on GET_REPOS_FAILED action', function() {
-   		var state, newState, response;
-
-   		beforeEach(function() {
-   			state = {
-           repos: {
-             message: "",
-             list: []
-           }
-   			}
-        newState = {
-           repos: {
-             message: ErrorMessages.GET_REPOS_FAILED,
-             list: []
-           }
-   			}
-   		});
-
-   		it('returns the state when no user found', function() {
-   			expect(reposReducer(state, {type: actionTypes.GET_REPOS_FAILED})).toEqual(newState)
-   		});
- 	   });
-
-	});
-});
+export default reposReducer;
